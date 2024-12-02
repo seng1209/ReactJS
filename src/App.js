574edbcfd2eb2fa2fa57c031ce0f9d19cff4c6df
Group1 <hongpink473@gmail.com>
2024-12-02 21:30:36 +0700

    React JS with PayPal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Purchase from "./context/Purchase";
+import Cart from "./context/Cart";
+
+import { CartProvider } from "./context/CartContext";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+      if (existingProduct) {
+        // If the product already exists, increase the quantity
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      // If it's a new product, add it to the cart with quantity 1
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/">
+              <Route path="" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="products" element={<Products />} />
+              <Route path="cart" element={<Cart />} />
+              <Route
+                path="purchase"
+                element={<Purchase addToCart={addToCart} />}
+              />
+              <Route path="*" element={"404"} />
+            </Route>
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    </CartProvider>
   );
 }
 
